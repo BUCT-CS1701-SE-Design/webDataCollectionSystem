529845fc1e32e1d8880285e46640f5c16865e7de
lvfengling <1975188506@qq.com>
2020-05-19 14:05:38 +0800

    修改collection109

--- MUSEUMS/spiders/collection109.py
+++ MUSEUMS/spiders/collection109.py
@@ -14,14 +14,17 @@ class Collection109Spider(scrapy.Spider):
     start_urls = ['http://www.ynnmuseum.com/products_list1.html']
 
     def parse(self, response):
-        li_list = response.xpath("//div[2]//div[2]/div/div/div[2]//ul/li")
+        li_list=response.xpath("/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/div[1]/ul/li")
         for li in li_list:
             item = collection75Item()
             item["museumID"] = 109
-            item["collectionName"] = li.xpath("./div[2]/ul/li/h1/strong/a/text()").extract_first()
-            item["collectionImage"] ="http://www.ynnmuseum.com" + str(li.xpath("./div[1]/div/a/img/@src").extract_first())
-            url ='http://www.ynnmuseum.com' + str(li.xpath("./div[1]/div/a/@href").extract_first())
-            yield scrapy.Request(
+            if li.xpath(".//div[1]/div/a/img/@src").extract_first() is not None:
+
+                item["collectionName"] =li.xpath("./div[2]/ul/li/h1/strong/a/@title").extract_first()
+            
+                item["collectionImage"] ='http://www.ynnmuseum.com'+li.xpath("./div[1]/div/a/img/@src").extract_first()
+                url='http://www.ynnmuseum.com'+li.xpath("./div[1]/div/a/@href").extract_first()
+                yield scrapy.Request(
                 url,
                 callback=self.parse_detail,
                 meta={"item": item}  # 传递参数
@@ -29,7 +32,8 @@ class Collection109Spider(scrapy.Spider):
 
     def parse_detail(self, response):
         item = response.meta["item"]
-        item["collectionIntroduction"] = response.xpath("normalize-space(//form[1]//p)").extract_first()
+        item["collectionIntroduction"] = response.xpath("/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]/div/div[1]/form[1]/div[2]/div/div/p[1]/text()").extract_first()
+        yield item 
+         
 
-        yield item
-
+        
